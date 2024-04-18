48fec0bc57fcf0d891cc6049e5f90632fd179c9c
gv <afairless@gmail.com>
2024-04-18 08:43:14 -0400

    add tests

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -26,3 +26,69 @@ def test_do_calculation_02():
 
     with pytest.raises(TypeError):
         result = do_calculation(input)
+
+
+def test_do_calculation_03():
+    """
+    Test invalid input 
+    """
+
+    input = -1
+
+    with pytest.raises(AssertionError):
+        result = do_calculation(input)
+
+
+def test_do_calculation_04():
+    """
+    Test invalid input 
+    """
+
+    input = -0.0001
+
+    with pytest.raises(AssertionError):
+        result = do_calculation(input)
+
+
+def test_do_calculation_05():
+    """
+    Test valid input
+    """
+
+    input = 0
+    result = do_calculation(input)
+    correct_result = 1
+    assert result == correct_result
+
+
+def test_do_calculation_06():
+    """
+    Test valid input
+    """
+
+    input = 1
+    result = do_calculation(input)
+    correct_result = 2
+    assert result == correct_result
+
+
+def test_do_calculation_07():
+    """
+    Test valid input
+    """
+
+    input = 2.51
+    result = do_calculation(input)
+    correct_result = 3.51
+    assert abs(result - correct_result) < 1e-6
+
+
+def test_do_calculation_08():
+    """
+    Test valid input
+    """
+
+    input = 100_002
+    result = do_calculation(input)
+    correct_result = 100_003
+    assert abs(result - correct_result) < 1e-6
